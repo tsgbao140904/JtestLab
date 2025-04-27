f29014ee9bce2724904641b874961a6c1e60679a
thaibao9715@gmail.com <thaibao9715@gmail.com>
2025-04-27 23:03:18 +0700

    init commit

--- src/a_Introductory/Fibonacci.java
+++ src/a_Introductory/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
 			case 0: return 0;
 			case 1: return 1;
 			default: return fib(n - 1) + fib(n - 2); // Đúng : không cộng thêm 1 nữa, chỉ cộng 2 giá trị fib trước đó.
-														// ⚠️ Sai: tự ý cộng thêm +1 → kết quả Fibonacci bị lệch. return (fib(n - 1) + fib(n - 2)) + 1;
+														//  Sai: tự ý cộng thêm +1 → kết quả Fibonacci bị lệch. return (fib(n - 1) + fib(n - 2)) + 1;
 		}
 	}
 }
